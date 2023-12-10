5e609eebf1ebe2f7ebbf2db0c582c9e4758a6cda
Burnus <github@burnus.net>
2023-12-10 10:55:06 +0100

    Fixed automation to include benchmarking

--- add.sh
+++ add.sh
@@ -6,9 +6,18 @@ if [ $# -ne 2 ]; then
 fi
 
 /home/burnus/.cargo/bin/cargo new --lib day${1}_${2}
+cp -r ../day00/benches day${1}_${2}/
 cp -r ../day00/tests day${1}_${2}/
 cp ../day00/src/lib.rs day${1}_${2}/src/
 cd day${1}_${2}
+echo '
+[dev-dependencies]
+criterion = "0.5.1"
+
+[[bench]]
+name = "test_benchmark"
+harness = false' >> Cargo.toml
+echo -e "use day${1}_${2}::run;\n$(cat benches/test_benchmark.rs)" > benches/test_benchmark.rs
 # echo 'intcode_processor = { path = "../common/intcode_processor" }' >> Cargo.toml
 aocf checkout $1
 aocf brief > challenge.txt
